7edb5b2b661bd0f30ef0f7f98cc7fa3dd726048b
Ankit Bisen <ankitbisen288@gmail.com>
2023-02-02 22:48:04 +0530

    ready for deploy

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,34 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
 const PORT = 5000 || process.env.PORT;
-const mongoDB = require('./db');
+const mongoDB = require("./db");
 const CreateUser = require("../backend/Routes/CreateUser.js");
 const DisplayData = require("../backend/Routes/DisplayData");
 const OrderData = require("../backend/Routes/OrderData");
-const cors = require('cors')
-const dotenv = require('dotenv');
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 dotenv.config();
 mongoDB();
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder up in production
+  app.use(express.static("../client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+  );
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.use(express.json());
-app.use('/api', CreateUser);
-app.use('/api', DisplayData);
-app.use('/api', OrderData);
+app.use("/api", CreateUser);
+app.use("/api", DisplayData);
+app.use("/api", OrderData);
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Example app listening on port ${PORT}`);
+});
